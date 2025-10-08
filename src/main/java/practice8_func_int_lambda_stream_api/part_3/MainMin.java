ef72ece88395567a2df1c788011804584a965216
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2025-10-08 23:20:46 +0300

    Fixes by feedback

--- src/main/java/practice8_func_int_lambda_stream_api/part_3/MainMin.java
+++ src/main/java/practice8_func_int_lambda_stream_api/part_3/MainMin.java
@@ -1,14 +1,16 @@
 package practice8_func_int_lambda_stream_api.part_3;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 public class MainMin {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 10, 1, 10);
-        Optional<Integer> minNumber = numbers.stream()
-                .reduce(Integer::min);
+        Integer minNumber = numbers.stream()
+                .min(Comparator.naturalOrder())
+                .get();
         System.out.println(minNumber);
     }
 }
