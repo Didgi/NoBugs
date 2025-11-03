6a9958ca2215a1f6881d90a22920fb25dae2a7a5
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2025-11-03 23:31:53 +0300

    Practice 11 homework for debug

---
+++ src/main/java/practice11_debug/DebugTask6.java
@@ -0,0 +1,14 @@
+package practice11_debug;
+
+public class DebugTask6 {
+    public static void main(String[] args) {
+        countdown(5);
+    }
+
+    public static void countdown(int n) {
+        System.out.println(n);
+        if (n > 1) {
+            countdown(n - 1);
+        }
+    }
+}
