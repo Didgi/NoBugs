767cb76ea132b6e6b1e2b3198c0dc804abf3d87e
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2026-03-25 18:35:18 +0300

    Implementation of senior api tests

--- src/main/java/api/models/UsersResponse.java
+++ src/main/java/api/models/UsersResponse.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UsersResponse {
+public class UsersResponse extends BaseModel {
     private int id;
     private String username;
     private String password;
