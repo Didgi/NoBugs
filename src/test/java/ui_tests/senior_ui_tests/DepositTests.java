7f99bb5b3e6da3d9627de31fb4876d161538a1a7
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2026-05-13 22:21:36 +0300

    Implementation of senior ui tests

--- src/test/java/ui_tests/senior_ui_tests/DepositTests.java
+++ src/test/java/ui_tests/senior_ui_tests/DepositTests.java
@@ -35,9 +35,21 @@ public class DepositTests extends UIBaseTestSenior {
 
         int expectedListSize = 2;
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем аккаунт созданного пользователя
         final int userAccount = SessionStorage.getUserAccountByUserNumber();
 
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Выбираем созданный аккаунт
+        // Проверяем отображение выбранного ID аккаунта и баланса аккаунта пользователя
+        // Проверяем отображение placeholder в поле Enter amount
+        // Вводим рандомное значение денег для пополнения аккаунта
+        // Нажимаем кнопку Deposit для пополнения аккаунта
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -83,11 +95,14 @@ public class DepositTests extends UIBaseTestSenior {
                 .inputAmountValue(expectedRandomMoney)
                 .clickDepositButton();
 
+        // Проверяем сообщение в модальном окне об успешности выполнения Deposit
         final String expectedAlertText = depositPage.expectedSuccessfullyDepositModalMessage(expectedRandomMoney, userAccount);
+        // Проверяем, что произошёл переход на главную страницу после выполнения Deposit
         depositPage.checkMessageFromModalPageAndAccept(expectedAlertText)
                 .getPage(MainPage.class)
                 .checkMainPageOpened();
 
+        // Проверяем через api, что операция Deposit выполнена успешно
         final double actualUserBalance = UserSteps.getUserBalance(authUserToken, userAccount);
         assertThat(actualUserBalance).isEqualTo(expectedRandomMoney);
 
@@ -106,10 +121,22 @@ public class DepositTests extends UIBaseTestSenior {
 
         int expectedListSize = 3;
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем первый аккаунт созданного пользователя
         final int userFirstAccount = SessionStorage.getUserAccountByUserNumber();
-        final int userSecondAccount = SessionStorage.getUserAccountByUserToken(authUserToken, 2);
+        //Получаем второй аккаунт созданного пользователя
+        final int userSecondAccount = SessionStorage.getUserAccountByUserToken(authUserToken,2);
 
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Проверяем отображение выбранного ID аккаунта и баланса аккаунта пользователя
+        // Проверяем отображение placeholder в поле Enter amount
+        // Вводим рандомное значение денег для пополнения аккаунта
+        // Нажимаем кнопку Deposit для пополнения аккаунта
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -118,11 +145,19 @@ public class DepositTests extends UIBaseTestSenior {
                 .checkSelectedAccountInList(authUserToken, userFirstAccount).
                 inputAmountValue(expectedRandomMoney).clickDepositButton();
 
+        // Проверяем сообщение в модальном окне об успешности выполнения Deposit
         final String expectedAlertText = depositPage.expectedSuccessfullyDepositModalMessage(expectedRandomMoney, userFirstAccount);
+        // Проверяем, что произошёл переход на главную страницу после выполнения Deposit
         depositPage.checkMessageFromModalPageAndAccept(expectedAlertText)
                 .getPage(MainPage.class)
                 .checkMainPageOpened();
 
+        // Проверяем лого страницы Deposit Money
+        // Открываем выпадающий список
+        // Выбираем второй аккаунт
+        // Проверяем отображение выбранного ID аккаунта и баланса аккаунта пользователя
+        // Вводим рандомное значение денег для пополнения аккаунта
+        // Нажимаем кнопку Deposit для пополнения аккаунта
         depositPage
                 .goToDepositPage()
                 .checkDepositPageOpened()
@@ -135,13 +170,17 @@ public class DepositTests extends UIBaseTestSenior {
         final String expectedSecondAlertText = depositPage
                 .expectedSuccessfullyDepositModalMessage(expectedRandomMoney, userSecondAccount);
 
+        // Проверяем сообщение в модальном окне об успешности выполнения Deposit
+        // Проверяем, что произошёл переход на главную страницу после выполнения Deposit
         depositPage.checkMessageFromModalPageAndAccept(expectedSecondAlertText)
                 .getPage(MainPage.class)
                 .checkMainPageOpened();
 
+        // Проверяем через api, что операция Deposit для первого аккаунта выполнена успешно
         final double actualFirstAccountBalance = UserSteps.getUserBalance(authUserToken, userFirstAccount);
         assertThat(actualFirstAccountBalance).isEqualTo(expectedRandomMoney);
 
+        // Проверяем через api, что операция Deposit для второго аккаунта выполнена успешно
         final double actualSecondAccountBalance = UserSteps.getUserBalance(authUserToken, userSecondAccount);
         assertThat(actualSecondAccountBalance).isEqualTo(expectedRandomMoney);
     }
@@ -242,9 +281,22 @@ public class DepositTests extends UIBaseTestSenior {
         double zeroBalance = 0.00;
         int expectedListSize = 2;
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем аккаунт созданного пользователя
         final int userAccount = SessionStorage.getUserAccountByUserNumber();
 
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Выбираем первый аккаунт
+        // Проверяем отображение выбранного ID аккаунта и баланса аккаунта пользователя
+        // Проверяем отображение placeholder в поле Enter amount
+        // Вводим отрицательное -0.01 значение денег для пополнения аккаунта
+        // Проверяем сообщение в модальном окне об ошибке выполнения Deposit и закрываем его
+        // Проверяем, что остались на той же странице
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -255,6 +307,7 @@ public class DepositTests extends UIBaseTestSenior {
                 .checkMessageFromModalPageAndAccept(DEPOSIT_ERROR_NEGATIVE_VALUE.getValue())
                 .checkDepositPageOpened();
 
+        // Проверяем через api, что операция Deposit не выполнена
         final double actualUserBalance = UserSteps.getUserBalance(authUserToken, userAccount);
         assertThat(actualUserBalance).isEqualTo(zeroBalance);
 
@@ -302,9 +355,22 @@ public class DepositTests extends UIBaseTestSenior {
         double zeroBalance = 0.00;
         int expectedListSize = 2;
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем аккаунт созданного пользователя
         final int userAccount = SessionStorage.getUserAccountByUserNumber();
 
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Выбираем первый аккаунт
+        // Проверяем отображение выбранного ID аккаунта и баланса аккаунта пользователя
+        // Проверяем отображение placeholder в поле Enter amount
+        // Вводим значение больше максимального допустимого значение денег для пополнения аккаунта
+        // Проверяем сообщение в модальном окне об ошибке выполнения Deposit
+        // Проверяем, что остались на той же странице
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -315,6 +381,7 @@ public class DepositTests extends UIBaseTestSenior {
                 .checkMessageFromModalPageAndAccept(DEPOSIT_ERROR_EXCEEDED_MAXIMUM_VALUE.getValue())
                 .checkDepositPageOpened();
 
+        // Проверяем через api, что операция Deposit не выполнена
         final double actualUserBalance = UserSteps.getUserBalance(authUserToken, userAccount);
         assertThat(actualUserBalance).isEqualTo(zeroBalance);
 
@@ -326,7 +393,13 @@ public class DepositTests extends UIBaseTestSenior {
     @Test
     @DisplayName("Негативный тест: проверка отображения ошибки при попытке нажатия 'Deposit' без выбора аккаунта")
     public void userSeesErrorMessageWhenClickDepositButtonWithoutAccount() {
-
+        
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Не выбираем никакой аккаунт и не вводим сумму для пополнения.
+        // Нажимаем кнопку Deposit для пополнения аккаунта
+        // Проверяем сообщение в модальном окне об ошибке выполнения Deposit
+        // Проверяем, что остались на той же странице
         depositPage
                 .open()
                 .checkDepositPageOpened()
@@ -366,9 +439,21 @@ public class DepositTests extends UIBaseTestSenior {
     @DisplayName("Негативный тест: проверка отображения ошибки при попытке нажатия 'Deposit' без указания суммы")
     public void userSeesErrorMessageWhenClickDepositButtonWithoutAmount() throws SQLException {
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем аккаунт созданного пользователя
         final int userAccount = SessionStorage.getUserAccountByUserNumber();
 
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Выбираем первый аккаунт
+        // Не вводим сумму для пополнения
+        // Нажимаем кнопку Deposit для пополнения аккаунта
+        // Проверяем сообщение в модальном окне об ошибке выполнения Deposit
+        // Проверяем, что остались на той же странице
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -378,6 +463,7 @@ public class DepositTests extends UIBaseTestSenior {
                 .checkMessageFromModalPageAndAccept(DEPOSIT_ERROR_WITHOUT_AMOUNT.getValue())
                 .checkDepositPageOpened();
 
+        // Проверяем через api, что операция Deposit не выполнена
         final List<UserTransactionsResponse> userTransactions = UserSteps.getUserTransactions(authUserToken, userAccount);
         assertThat(userTransactions).isEmpty();
 
@@ -393,9 +479,22 @@ public class DepositTests extends UIBaseTestSenior {
             "хотя ранее он был выбран")
     public void userSeesErrorMessageWhenClickDepositButtonWithoutAccountWhenAccountWasChooseBefore() throws SQLException {
 
+        //Получаем токен созданного пользователя
         final String authUserToken = SessionStorage.getUserTokenFromStorage();
+        //Получаем аккаунт созданного пользователя
         final int userAccount = SessionStorage.getUserAccountByUserNumber();
 
+
+        //Открываем страницу выполнения депозита
+        // Проверяем лого страницы Deposit Money
+        // Проверяем значение по-умолчанию в выпадающем списке Select Account:
+        // Проверяем размер списка
+        // Открываем выпадающий список
+        // Выбираем первый аккаунт
+        // Не вводим сумму для пополнения
+        // Нажимаем кнопку Deposit для пополнения аккаунта
+        // Проверяем сообщение в модальном окне об ошибке выполнения Deposit
+        // Проверяем, что остались на той же странице
         depositPage.open()
                 .checkDepositPageOpened()
                 .checkDefaultValueInAccountList()
@@ -407,6 +506,7 @@ public class DepositTests extends UIBaseTestSenior {
                 .checkMessageFromModalPageAndAccept(DEPOSIT_ERROR_WITHOUT_REQUIRED_FIELDS.getValue())
                 .checkDepositPageOpened();
 
+        // Проверяем через api, что операция Deposit не выполнена
         final List<UserTransactionsResponse> userTransactions = UserSteps.getUserTransactions(authUserToken, userAccount);
         assertThat(userTransactions).isEmpty();
 
