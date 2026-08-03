1dbf8a3fa961d75c2166741a05a3ac11e7a9bf1d
Aleksei Kokarev <alexey-kokarev@yandex.ru>
2026-08-03 18:49:23 +0300

    1. set up ci/cd

---
+++ infra/push-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+source Docker.env
+
+ENV=SHORT_COMMIT_HASH
+echo ">>> Получен хэш коммита: ${SHORT_COMMIT_HASH}"
+
+echo ">>> Авторизация в docker"
+echo "${DOCKER_TOKEN}" | docker login -u ${DOCKER_USERNAME} --password-stdin
+
+IMAGE_NAME=nbank-tests
+#SHORT_COMMIT_HASH=${GITHUB_SHA::7}
+DOCKER_IMAGE="${DOCKER_USERNAME}/${IMAGE_NAME}:${SHORT_COMMIT_HASH}"
+
+echo ">>> Старт сборки докер образа"
+docker build -t ${DOCKER_IMAGE} .
+
+echo ">>> Собран докер образ с именем: ${DOCKER_IMAGE}"
+echo ">>> Старт публикации образа в dockerhub"
+
+docker push ${DOCKER_IMAGE};
+
+echo ">>> Образ с именем: ${DOCKER_IMAGE} опубликован в dockerhub"
+echo ">>> Скачать данный образ можно выполнив команду: docker pull ${DOCKER_IMAGE}"
